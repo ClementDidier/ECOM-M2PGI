e37b1c9e44a30cbb6361bc32ce09afe5a0e022fc
Clément D <didiercl@outlook.fr>
2017-09-18 10:31:02 +0200

    Mise à jour des entêtes des réponses
    * Mise à jour pour accepter les "

---
+++ src/server/ecom/ecom-web/src/main/java/utils/CORSFilter.java
@@ -0,0 +1,51 @@
+package utils;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class CORSFilter
+ */
+public class CORSFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public CORSFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// Cast pour l'accès aux méthodes de modification de l'entête de la réponse
+		final HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+		httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
