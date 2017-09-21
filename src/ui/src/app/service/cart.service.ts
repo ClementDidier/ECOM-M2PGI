53f57e0d59e763992882be739ff2d6b29a56a676
Steffunky <stephen@stephen.ovh>
2017-09-21 09:49:33 +0000

    Chaine angular pour le panier ok

---
+++ src/ui/src/app/service/cart.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { CartItem } from '../model/cart-item';
+import { environment as env } from '../../environments/environment';
+
+@Injectable()
+export class CartService {
+
+  constructor(private http: Http) { }
+
+  public getCart(): Observable<CartResponse> {
+    return this.http.get(`${env.appUrl}/cart`)
+      .map(res => {
+        const body: any = res.json();
+        return { err: null, cartItems: body};
+      })
+      .catch(err => {
+        console.log('Server error: ' + JSON.stringify(err, null, 2));
+        return Observable.of({err: err, bungalows: null});
+      })
+      ;
+    }
+}
+
+export interface CartResponse {
+  err: any;
+  cartItems: CartItem[];
+}
